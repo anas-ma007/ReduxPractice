20e586a699ed3b380dc9cb1662baf29eccbca1c9
anas-ma007 <anaschelakkara786@gmail.com>
2024-07-14 13:11:31 +0530

    some changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,8 +7,8 @@ const Body = () => {
                     <p className='font-bold underline text-xl hidden md:block from-neutral-700'>Body Component </p>
                     <p className='font-bold underline text-base md:hidden   from-neutral-700'>Body Component (Mob Screen) </p>
                     <p className='mt-4 font-semibold text-base md:text-xl '>Count 0</p>
-                    <button className="bg-slate-600 hover:bg-slate-800 text-base md:text-xl px-6 md:px-19 py-1 md:py-1.5 m-2 md:m-3 rounded font-semibold text-white"> Plus </button>
-                    <button className="bg-slate-600 hover:bg-slate-800 text-base md:text-xl px-6 md:px-19 py-1 md:py-1.5 m-2 md:m-3 rounded font-semibold text-white"> Minus </button>
+                    <button className="bg-slate-600 hover:bg-slate-800 text-base md:text-xl px-6 md:px-19 py-1 md:py-1.5 m-2 md:m-3 rounded font-semibold text-white"> Increase </button>
+                    <button className="bg-slate-600 hover:bg-slate-800 text-base md:text-xl px-6 md:px-19 py-1 md:py-1.5 m-2 md:m-3 rounded font-semibold text-white"> Decrease </button>
                 </div>
             </div>
         </div>
